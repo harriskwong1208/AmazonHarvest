fb1f6589b6dcf9b2c21489b99bda3fe84bba2ecf
harris <harriskwong120898@gmail.com>
2024-05-30 20:03:42 -0400

    Imported libraries, included link to web scrap

---
+++ Index.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+import time 
+import datetime
+import smtplib
+import requests
+
+URL = 'https://www.amazon.com/Funny-Data-Systems-Business-Analyst/dp/B07FNW9FGJ/ref=sr_1_3?dchild=1&keywords=data%2Banalyst%2Btshirt&qid=1626655184&sr=8-3&customId=B0752XJYNL&th=1'
+
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36", "Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
+
+page = requests.get(URL, headers=headers)
+
